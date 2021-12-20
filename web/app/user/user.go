e1c62c22b1e5f50076d3f99a0a8a3da8949fc64a
Denise Case <denisecase@gmail.com>
2021-12-19 19:21:08 -0600

    initial auth user page

--- web/app/user/user.go
+++ web/app/user/user.go
@@ -12,5 +12,6 @@ func Handler(ctx *gin.Context) {
 	log.Print("LOGGED IN USER HANDLER: starting")
 	session := sessions.Default(ctx)
 	profile := session.Get("profile")
-	ctx.HTML(http.StatusOK, "user.html", profile)
+	//ctx.HTML(http.StatusOK, "user.html", profile)
+	ctx.HTML(http.StatusOK, "user.tmpl.html", profile)
 }
